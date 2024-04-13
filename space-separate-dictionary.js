01adb447ee1560576259af321479b0ca024c08d4
Madan <42720560+hegdeMadan@users.noreply.github.com>
2024-04-13 14:35:35 +0000

    update space-separate-dictionary

--- space-separate-dictionary.js
+++ space-separate-dictionary.js
@@ -1,38 +1,29 @@
 // Given a dictionary and a string. Return if the string can be space separated into words
 
-const string = "helloworldfolks"; // "hello world folks"
-const dictionary = {
-    "folk": true,
-    "folks": true,
-    "he": true,
-    "hell": true,
-    "hello": true,
-    "world": true
-}
+const dictionary = new Set(["folk", "folks", "he", "hell", "low", "hello", "world"]);
+
 
 const spaceSeparate = (string, dict) => {
-    let n = string.length;
+  let n = string.length;
 
-    for (let i = 0; i < n; i++) {
-        let stringPart1 = string.slice(0,i);
-        let stringPart2 = string.slice(i,n);
-    
-        if (dict[stringPart1] != undefined) {
-        if (dict[stringPart2] !== undefined) {
-            return stringPart1+ ' '+stringPart2;
-        } else {
-            stringPart2 = spaceSeparate(stringPart2,dict);
+  for (let i = 0; i < n; i++) {
+    let stringPart1 = string.slice(0, i);
+    let stringPart2 = string.slice(i, n);
 
-            if (stringPart2 !== null) {
+    if (dict.has(stringPart1)) {
+      if (dict.has(stringPart2)) {
+        return stringPart1 + " " + stringPart2;
+      } else {
+        stringPart2 = spaceSeparate(stringPart2, dict);
 
-                return stringPart1+' '+stringPart2;
-            }
+        if (stringPart2 !== null) {
+          return stringPart1 + " " + stringPart2;
         }
-        }
+      }
     }
-    return null;
-}
+  }
+  return null;
+};
 
-console.log(spaceSeparate(string,dictionary));
-
+console.log(spaceSeparate("helloworldfolks", dictionary)); // "hello world folks"
 
